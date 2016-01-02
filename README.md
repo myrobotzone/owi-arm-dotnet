da4dde88ab1271263482cbcf7fd4ab11f355b634
myrobotzone <jonas.hodel@gmail.com>
2016-01-02 16:23:23 +0100

    Added sample application. Work in progress.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # owi-arm-dotnet
+
+A .NET library that implements the [OWI-007 Robotic Arm parallel port protocol](http://notbrainsurgery.livejournal.com/38622.html). Tested on Windows 10 using [http://www.maplin.co.uk/p/usb-controlled-robotic-arm-kit-a37jn](USB Controlled Robotic Arm Kit)
---
+++ owi-arm-dotnet/sample.app/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Windows.Input;
+
+namespace sample.app.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            this.StopCommand = new RelayCommand(() => this.OnStopCommand());
+        }
+
+
+        private int elbowSliderValue;
+
+        public int ElbowSliderValue
+        {
+            get
+            {
+                return elbowSliderValue;
+            }
+            set
+            {
+                elbowSliderValue = value;
+                this.RaisePropertyChanged(() => ElbowSliderValue);
+            }
+        }
+
+        public ICommand StopCommand { get; private set; }
+
+        private void OnStopCommand()
+        {
+            ElbowSliderValue = 0;
+        }
+    }
+}
