c889ac7e871d0f2cb2c5c7bd977d241327281e25
myrobotzone <jonas.hodel@gmail.com>
2016-01-03 07:12:55 +0100

    Enabled nuget restore. Introduced Command, which replace existing command implementation.

---
+++ owi-arm-dotnet/owi-arm-dotnet-test/CommandTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using owi_arm_dotnet;
+
+namespace owi_arm_dotnet_test
+{
+    [TestClass]
+    public class OwiCommandTest
+    {
+        [TestMethod]
+        public void LedOn_LedWasOff_LedBitIsSet()
+        {
+            var command = new Command();
+
+            command.LedOn();
+
+            Assert.IsTrue(IsBitSet(command.LedByte, 0));
+        }
+
+
+        [TestMethod]
+        public void LedOff_LedWasOn_LedBitIsNotSet()
+        {
+            var command = new Command();
+
+            command.LedOn();
+            command.LedOff();
+
+            Assert.IsFalse(IsBitSet(command.LedByte, 0));
+        }
+
+        [TestMethod]
+        public void ElbowUp_ElbowWasStoped_ElbowUpBitIsSet()
+        {
+            var command = new Command();
+
+            command.ElbowUp();
+
+            Assert.IsTrue(IsBitSet(command.ElbowByte, 4));
+        }
+
+        [TestMethod]
+        public void ElbowDown_ElbowWasMovingUp_ElbowDownBitIsSet()
+        {
+            var command = new Command();
+
+            command.ElbowUp();
+            command.ElbowDown();
+            
+            Assert.IsFalse(IsBitSet(command.ElbowByte, 4));
+            Assert.IsTrue(IsBitSet(command.ElbowByte, 5));
+        }
+
+        [TestMethod]
+        public void ElbowStop_ElbowWasDown_ElbowBitsAreReset()
+        {
+            var command = new Command();
+
+            command.ElbowDown();
+            command.ElbowStop();
+
+            Assert.IsFalse(IsBitSet(command.ElbowByte, 4));
+            Assert.IsFalse(IsBitSet(command.ElbowByte, 5));
+        }
+
+        bool IsBitSet(byte b, int pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+    }
+}
---
+++ owi-arm-dotnet/owi-arm-dotnet/Command.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace owi_arm_dotnet
+{
+    public class Command
+    {
+        BitArray ledArray = new BitArray(8);
+        BitArray elbowArray = new BitArray(8);
+
+        public byte LedByte
+        {
+            get { return ConvertToByte(this.ledArray); }
+        }
+
+        public byte ElbowByte
+        {
+            get { return ConvertToByte(this.elbowArray); }
+        }
+
+        public void LedOn()
+        {
+            this.ledArray.Set(0, true);
+        }
+
+        public void LedOff()
+        {
+            this.ledArray.Set(0, false);
+        }
+
+        public void ElbowUp()
+        {
+            this.ElbowStop();
+            this.elbowArray.Set(4, true);
+        }
+
+        public void ElbowDown()
+        {
+            this.ElbowStop();
+            this.elbowArray.Set(5, true);
+        }
+
+        public void ElbowStop()
+        {
+            this.elbowArray.Set(4, false);
+            this.elbowArray.Set(5, false);
+        }
+
+        private byte ConvertToByte(BitArray bits)
+        {
+            if (bits.Count != 8)
+            {
+                throw new ArgumentException("bits");
+            }
+            byte[] bytes = new byte[1];
+            bits.CopyTo(bytes, 0);
+            return bytes[0];
+        }
+    }
+}
